b029da3c0d1b200e94ebe822748291dd4c6ef153
TheNexusAvenger <zpcook@googlemail.com>
2019-10-06 22:04:59 -0400

    Added remote branches request

---
+++ server/NexusGit/Git/RepositoryActions/RemoteBranches.cs
@@ -0,0 +1,55 @@
+/*
+ * TheNexusAvenger
+ * 
+ * Gets the remote branches in a repository.
+ */
+
+using NexusGit.FileIO;
+
+namespace NexusGit.Git.RepositoryActions
+{
+    /*
+     * Class representing a remote branches action.
+     */
+    public class RemoteBranches : RepositoryAction
+    {
+        private string Command;
+
+        /*
+         * Creates a local branches action object.
+         */
+        public RemoteBranches(Repository repository) : base(repository)
+        {
+            this.Command = "branch -r";
+        }
+
+        /*
+         * Returns the command to run.
+         */
+        public override string GetCommand()
+        {
+            return this.Command;
+        }
+
+        /*
+         * Performs the action and returns the response.
+         */
+        public override GitResponse CompleteAction(ExecutableOutput executableOutput)
+        {
+            // Create the response.
+            GitResponse response = new GitResponse();
+            
+            // Add the branches.
+            foreach (string line in executableOutput)
+            {
+                if (line != "")
+                {
+                    response.AddResponse(line);
+                }
+            }
+
+            // Return the response.
+            return response;
+        }
+    }
+}
