7df7d1a91a6bcec9b18a76cbb8aa1556ca0e9b91
TheNexusAvenger <zpcook@googlemail.com>
2019-10-13 00:39:33 -0400

    Added list remotes functional test

---
+++ server/FunctionalTests/Tests/Rojo04/ListRemotes.py
@@ -0,0 +1,36 @@
+"""
+TheNexusAvenger
+
+Tests that the list remotes request acts correctly.
+"""
+
+import NexusGitFunctionalTest
+
+
+
+"""
+Tests the list remotes as a standalone request.
+"""
+class Rojo04TestListRemotesAsStandaloneRequest(NexusGitFunctionalTest.NexusGitFunctionalTest):
+    """
+    Setup for the test.
+    """
+    def setupTest(self):
+        self.workspace.writeFile("rojo.json","{\"name\": \"Nexus Git Repository\",\"servePort\": 30000,\"partitions\": {\"src\": {\"path\": \"src\",\"target\": \"ReplicatedStorage.NexusGit\"},\"test\": {\"path\": \"test\",\"target\":\"ReplicatedStorage.NexusGitTest\"}}}")
+
+        # Initialize the git repository.
+        self.workspace.runCommand("git init")
+        self.workspace.runCommand("git remote add origin1 https://github.com/TheNexusAvenger/Nexus-Instance")
+        self.workspace.runCommand("git remote add origin2 https://github.com/TheNexusAvenger/Nexus-Instance")
+
+    """
+    Runs the test.
+    """
+    def runTest(self):
+        # Wait for it to initialize.
+        self.setPortNumber(30000)
+        self.waitForInitialization()
+
+        # Assert the remotes are correct.
+        response = self.sendGETRequest("/listremotes")
+        self.assertEquals(response,"[\"origin1\",\"origin2\"]")
