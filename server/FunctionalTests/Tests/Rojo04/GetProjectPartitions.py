80d36f11cca72c85f8e2f8436852f08476c7628e
TheNexusAvenger <zpcook@googlemail.com>
2019-11-10 00:33:23 -0500

    Modified tests to use new action URLs

---
+++ server/FunctionalTests/Tests/Rojo04/GetProjectPartitions.py
@@ -0,0 +1,31 @@
+"""
+TheNexusAvenger
+
+Tests that the get project partitions request acts correctly.
+"""
+
+import NexusGitFunctionalTest
+
+
+
+"""
+Tests the project partitions as a standalone request.
+"""
+class Rojo04TestProjectPartitionsAsStandaloneRequestMultipleFile(NexusGitFunctionalTest.NexusGitFunctionalTest):
+    """
+    Setup for the test.
+    """
+    def setupTest(self):
+        self.workspace.writeFile("rojo.json","{\"name\": \"Nexus Git Repository\",\"servePort\": 20011,\"partitions\": {\"src\": {\"path\": \"src\",\"target\": \"ReplicatedStorage.NexusGit\"},\"test\": {\"path\": \"test\",\"target\":\"ReplicatedStorage.NexusGitTest\"}}}")
+
+    """
+    Runs the test.
+    """
+    def runTest(self):
+        # Wait for it to initialize.
+        self.setPortNumber(20011)
+        self.waitForInitialization()
+
+        # Send a standalone request and get the response.
+        response = self.sendGETRequest("/getprojectpartitions")
+        self.assertEquals(response,"{\"src\":\"ReplicatedStorage.NexusGit\",\"test\":\"ReplicatedStorage.NexusGitTest\"}")
